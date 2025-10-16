c220a57a58e1bc0658c8ded3029387c15854f27d
anastasiail <anastasiailasova0@gmail.com>
2025-10-16 18:11:10 +0300

    Update formula.c

--- src/formula.c
+++ src/formula.c
@@ -106,3 +106,19 @@ void find_and_print_formula(int a, int b, int c, int d) {
         }
     }
 }
+
+int main() {
+    printf("Тестирование:\n\n");
+    
+    test_fibonacci();
+    test_geometric_progression();
+    test_single_root();
+    test_complex_roots();
+    test_negative_values();
+    test_zero_sequence();
+    test_arithmetic_progression();
+    test_edge_case_zero_b();
+    test_edge_case_zero_a_b();
+
+    return 0;
+}
